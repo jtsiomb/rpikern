d80ebb8add60dd01b37d21c21a1a9d971f0f9dce
John Tsiombikas <nuclear@member.fsf.org>
2020-11-15 09:26:55 +0200

    initialize timer

--- src/timer.c
+++ src/timer.c
@@ -6,5 +6,10 @@ volatile unsigned long num_ticks;
 
 void timer_init(void)
 {
+	/* clear all 4 timer interrupt bits */
+	STM_CTL_REG = 0xf;
+	/* setup first timer interrupt */
+	STM_CMP1_REG = STM_LCNT_REG + 4000;
+
 	enable_gpu_irq(IRQ_GPU_TIMER1);
 }
